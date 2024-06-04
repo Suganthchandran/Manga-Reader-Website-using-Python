aa488772df91c1c4773dde49b447d45e8cc46322
Suganth <suganthchandran07@gmail.com>
2024-06-04 13:35:46 +0530

    Updated

--- manga/admin.py
+++ manga/admin.py
@@ -3,6 +3,15 @@ from .models import *
 
 # Register your models here.
 
+class PDFFileInline(admin.TabularInline):
+    model = Mangas.pdf_files.through
+    extra = 1
+
+class PDFFileAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(PDFFile, PDFFileAdmin)
+
 admin.site.register(Genre)
 admin.site.register(Mangas)
 
